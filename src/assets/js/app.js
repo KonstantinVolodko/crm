7624a45c67464f1906e4ca9b044bbb61dafc0fa2
KonstantinVolodko <volodko2000@bk.ru>
2022-09-16 17:47:51 +0300

    hzCacoyCommit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 //= components/
+
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    setWrapperSize: false,
+    navigation: {
+        nextEl: ".arrowRight",
+        prevEl: ".arrowLeft",
+      },
+  });
   
 })
 
