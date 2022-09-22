0d82fcfb0f658602ddda014cf84125d708326730
KonstantinVolodko <volodko2000@bk.ru>
2022-09-22 17:43:08 +0300

    kfkfkfkfkfkfkkff

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -40,6 +40,74 @@ let swiper4 = new Swiper(".keysesSwiper", {
     prevEl: ".keysesArrowRight",
   },
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".marqSpan", {
+//   scrollTrigger: {
+//     trigger: ".marqSpan",
+//     start: "top 80%",
+//     end: "top 30%",
+//     scrub: true,
+//     toggleActions: "restart none none none",
+//   }
+// })
+
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.blueBox',
+//     markers: true,
+//     start: 'top 80%',
+//     end: 'top 10%',
+//     scrub: true,
+//   }
+// });
+
+
+// tl.to('.blueBox', {x: 500, duration: 5})
+//   .to('.blueBox', {y: 200, duration: 3})
+//   .to(".blueBox", {x: 0, duration: 2})
+
+gsap.to('.marqSpan', {
+  x: 1500,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".marqSpan",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: 'restart reverse none none',
+    scrub: true,
+
+  }
+})
+
+gsap.to('.siteLineSpan', {
+  x: -3000,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".siteLineSpan",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: 'restart reverse none none',
+    scrub: true,
+
+  }
+})
+
+gsap.to('.businessLineSpan', {
+  x: 1000,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".businessLineSpan",
+    start: "top 80%",
+    end: "top 30%",
+    toggleActions: 'restart reverse none none',
+    scrub: true,
+
+  }
+})
+
+
   
 })
 
